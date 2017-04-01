014d2cde91e3a8e1cb1164f63183dca2e398ce10
Kiebek <mateuszkiebala@gmail.com>
2017-04-01 14:43:20 +0200

    Extended zad1, zad1 from labs

--- klasyfikacja.R
+++ klasyfikacja.R
@@ -1,29 +1,25 @@
 library(tree)
 
 # Zadanie 1
-filteredIris <- iris[iris$Petal.Width > 1.5, ]
-filteredIris$Species
+iris[iris$Petal.Width > 1.5, ]$Species
+
 
 # Zadanie 2
 countClassificationError <- function (n) {
   iris <- iris[sample(nrow(iris)),]
   sample_size <- dim(iris)[1] / n
-  # generate n groups from iris table
-  groups <- split(iris, rep(1:n, each = sample_size))
-  message(sprintf("Iteration error: %f", classification_error <- (sum(sapply(groups, getGroupError)) / length(groups))))
-  classification_error
+  sum_error <- 0
+  
+  for (i in (1:n)){
+    sam <- (((i-1)*sample_size + 1) : (i*sample_size))
+    train <- iris[-sam,]
+    test  <- iris[sam,]
+    iris.tr <- tree(Species ~., train)
+    fit <- predict(iris.tr, test, type="class")
+    sum_error <- sum_error + (length(sam) - sum(diag(table(test$Species, fit)))) / (length(sam))
+  }
+  print (sum_error / n)
 }
 
-getGroupError <- function (g) {
-  m <- dim(g)[1]
-  val <- sample(1:m, size = round(m/3), replace = FALSE, prob = rep(1/m, m))
-  g.learn <- g[-val,]
-  g.valid <- g[val,]
-  g.tr <- tree(Species ~., g.learn)
-  fit <- predict(g.tr, g.valid, type="class")
-  error <- (dim(g.valid)[1] - sum(diag(table(g.valid$Species, fit)))) / dim(g.valid)[1]
-}
-
-n = 1
-iterations = 10
-print (sum(sapply(rep(n, each=iterations), countClassificationError)) / iterations)
+n = 10
+countClassificationError(n)
--- zad1/zad1.R
+++ zad1/zad1.R
@@ -17,7 +17,9 @@ calculate <- function (n) {
   print (n)
   sample.size = nrow(data) / n
   tree.res = data.frame(auc = vector(length = n))
-  knn.res = data.frame(auc = vector(length = n))
+  knn10.res = data.frame(auc = vector(length = n))
+  knn50.res = data.frame(auc = vector(length = n))
+  knn100.res = data.frame(auc = vector(length = n))
   bayes.res = data.frame(auc = vector(length = n))
   for (i in 1:n) {
     sample = (((i-1)*sample.size + 1) : (i*sample.size))
@@ -30,10 +32,20 @@ calculate <- function (n) {
     preds = prediction(as.numeric(tree.prediction) - 1, data.test$D)
     tree.res$auc[i] = slot(performance(preds, measure="auc"), "y.values")
     
-    # KNN
-    knn.prediction = knn(data.train[,1:10], data.test[,1:10], data.train$D, k=50)
-    preds = prediction(as.numeric(knn.prediction) - 1, data.test$D)
-    knn.res$auc[i] = slot(performance(preds, measure="auc"), "y.values")
+    # KNN k = 10
+    knn10.prediction = knn(data.train[,1:10], data.test[,1:10], data.train$D, k=10)
+    preds = prediction(as.numeric(knn10.prediction) - 1, data.test$D)
+    knn10.res$auc[i] = slot(performance(preds, measure="auc"), "y.values")
+    
+    # KNN k = 50
+    knn50.prediction = knn(data.train[,1:10], data.test[,1:10], data.train$D, k=50)
+    preds = prediction(as.numeric(knn50.prediction) - 1, data.test$D)
+    knn50.res$auc[i] = slot(performance(preds, measure="auc"), "y.values")
+    
+    # KNN k = 100
+    knn100.prediction = knn(data.train[,1:10], data.test[,1:10], data.train$D, k=100)
+    preds = prediction(as.numeric(knn100.prediction) - 1, data.test$D)
+    knn100.res$auc[i] = slot(performance(preds, measure="auc"), "y.values")
     
     # Naive Bayes
     naiveBayesModel = naiveBayes(as.factor(D)~., data.train)
@@ -42,13 +54,23 @@ calculate <- function (n) {
     bayes.res$auc[i] = slot(performance(preds, measure="auc"), "y.values")
   }
   
-  c(mean(as.numeric(tree.res$auc)), mean(as.numeric(knn.res$auc)), mean(as.numeric(bayes.res$auc)))
+  c(mean(as.numeric(tree.res$auc)),
+    mean(as.numeric(knn10.res$auc)),
+    mean(as.numeric(knn50.res$auc)),
+    mean(as.numeric(knn100.res$auc)),
+    mean(as.numeric(bayes.res$auc)))
 }
 
+# znormalizowac dane
+# accuracy
 res <- sapply(c(2,4,8,10,16,20,25,32,40,50,100,200), calculate)
 print ("Decision tree auc summary:")
 print (summary(res[1,]))
-print ("KNN auc summary:")
+print ("KNN (k=10) auc summary:")
 print (summary(res[2,]))
-print ("Naive Bayes auc summary:")
+print ("KNN (k=50) auc summary:")
 print (summary(res[3,]))
+print ("KNN (k=100) auc summary:")
+print (summary(res[4,]))
+print ("Naive Bayes auc summary:")
+print (summary(res[5,]))
